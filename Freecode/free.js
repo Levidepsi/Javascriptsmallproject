3deb7c52debde4ea03851257d480f73066b3b4a3
Levidepsi <69233374+Levidepsi@users.noreply.github.com>
2021-06-21 19:59:18 +0800

    Add files via upload

---
+++ Freecode/free.js
@@ -0,0 +1,112 @@
+// Lessons in free code camp
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3 ;
+c *= 10;
+
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a  /= 12;
+b /= 4;
+c  /= 11;
+
+var myFirstName = 'Levi';
+var myLastName = 'Rosario';
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\".";  // Change this line
+
+console.log(myStr)
+
+var myStr = "This is the start." + " This is the end."; // Change this line
+
+console.log(myStr)
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+
+myStr += "This is the second sentence."
+
+console.log(myStr)
+
+// Change code below this line
+
+var someAdjective = "very hard!";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
+
+console.log(lastNameLength)
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+console.log(lastName)
+console.log(firstLetterOfLastName)
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
+
+console.log(lastName)
+
+console.log(thirdLetterOfLastName)
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+console.log(secondToLastLetterOfLastName)
+
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "There is a "+ myAdjective + " "  + myNoun + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks)
+
+// Only change code below this line
+var myArray = ['Levi', 7];
